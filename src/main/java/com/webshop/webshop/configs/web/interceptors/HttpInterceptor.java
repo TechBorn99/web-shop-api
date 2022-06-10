3c966253593236c825595d0aad6e717441951be2
Stefan Radojević <57040379+Stefi99R@users.noreply.github.com>
2022-06-10 05:45:01 +0200

    Added web configuration (#6)
    * Added request interceptor
    Interceptor for logging requests and their results
    
    * Added web config
    Includes interceptor config and CORS filters
    
    * Removed unnecessary lines of code

---
+++ src/main/java/com/webshop/webshop/configs/web/interceptors/HttpInterceptor.java
@@ -0,0 +1,50 @@
+package com.webshop.webshop.configs.web.interceptors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Component
+public class HttpInterceptor implements HandlerInterceptor {
+
+    Logger logger = LoggerFactory.getLogger("REQUEST");
+
+    @Override
+    public boolean preHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) throws Exception {
+        logger.info("~> ["
+                + new Date().toString()
+                + "] - REQUEST: "
+                + request.getRequestURI() + " | "
+                + request.getMethod());
+        return true;
+    }
+    @Override
+    public void postHandle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object object,
+            ModelAndView modelAndView
+    ) {
+        logger.info("✅ [" + new Date().toString() + "] - SUCCESSFUL: " +
+                request.getRequestURI() + " " + request.getQueryString() +
+                " | " + request.getMethod() + " | " + response.getStatus());
+    }
+
+    @Override
+    public void afterCompletion(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object object,
+            Exception exception
+    ) {}
+
+}
