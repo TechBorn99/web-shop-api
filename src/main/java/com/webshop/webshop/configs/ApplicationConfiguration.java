10e1a5a473748d310c65d43fa0419cb46bb38b6a
Stefan Radojević <57040379+Stefi99R@users.noreply.github.com>
2022-06-10 05:27:41 +0200

    Added bcrypt bean (#4)

--- src/main/java/com/webshop/webshop/configs/ApplicationConfiguration.java
+++ src/main/java/com/webshop/webshop/configs/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.webshop.webshop.configs;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -10,5 +11,10 @@ public class ApplicationConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
