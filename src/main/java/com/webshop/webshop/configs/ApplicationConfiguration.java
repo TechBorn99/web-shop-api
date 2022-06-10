4a7e60aee5e8c1519a3d24b00274c3815517201a
Stefan Radojevic <stefan.radojevic@mca>
2022-06-10 05:20:41 +0200

    Added model mapper bean

---
+++ src/main/java/com/webshop/webshop/configs/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package com.webshop.webshop.configs;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
