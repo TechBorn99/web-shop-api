2a3ea4794f450b33bac2698b0425818c1eb54647
Stefan Radojević <57040379+Stefi99R@users.noreply.github.com>
2022-06-12 02:32:00 +0200

    Added mail service (#10)

---
+++ src/main/java/com/webshop/webshop/services/mail/MailService.java
@@ -0,0 +1,48 @@
+package com.webshop.webshop.services.mail;
+
+import com.webshop.webshop.utils.exceptions.consts.ExceptionErrorCodeType;
+import com.webshop.webshop.utils.exceptions.types.EmailNotSentException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private MailProperties mailProperties;
+
+    @Async
+    private void sendHtmlMail(String[] sendTo, String subject, String body) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        MimeMessageHelper mimeMessageHelper;
+
+        try {
+            mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+            mimeMessageHelper.setTo(sendTo);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(body, true);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException ex) {
+            throw new EmailNotSentException(
+                    ExceptionErrorCodeType.EMAIL_NOT_SENT,
+                    "Email was not sent."
+            );
+        }
+    }
+}
--- src/main/java/com/webshop/webshop/utils/exceptions/consts/ExceptionErrorCodeType.java
+++ src/main/java/com/webshop/webshop/utils/exceptions/consts/ExceptionErrorCodeType.java
@@ -2,7 +2,9 @@ package com.webshop.webshop.utils.exceptions.consts;
 
 public enum ExceptionErrorCodeType {
 
-    EMAIL_NOT_FOUND(101);
+    EMAIL_NOT_FOUND(101),
+    EMAIL_NOT_SENT(401);
+
 
     public final int CODE;
 
